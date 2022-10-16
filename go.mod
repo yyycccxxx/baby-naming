ce9fb033d46795b1deb5657fe17c33dfbf586365
Chenxu Yang <ychenxu@vmware.com>
2022-10-16 17:12:25 +0800

    added branch v5

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yyycccxxx/baby-naming/v4
+module github.com/yyycccxxx/baby-naming/v5
 
 go 1.18
--- naming/naming.go
+++ naming/naming.go
@@ -1,18 +1,23 @@
 package naming
 
-import (
-  "math/rand"
-  "time"
-)
-
-func CreateBabyName(male bool) string {
+func CreateBabyName(male bool, minLen int) string {
   male_names := make([]string, 0)
   male_names = append(male_names, "Mike", "Tim", "Jianguo", "William", "Jack", "Jay", "Steven")
   female_names := make([]string, 0)
   female_names = append(female_names, "Elisabeth", "Jenifer", "Olivia")
-  rand.Seed(time.Now().Unix())
+  
   if male {
-    return  male_names[rand.Intn(len(male_names))]
+    for i :=1; i < len(male_names); i++ {
+      if len(male_names[i]) >= minLen {
+        return male_names[i]
+      } 
+    }
+  } else {
+    for i :=1; i < len(female_names); i++ {
+      if len(female_names[i]) >= minLen {
+        return female_names[i]
+      }
+    }
   }
-  return female_names[rand.Intn(len(female_names))]
+  return ""
 }
